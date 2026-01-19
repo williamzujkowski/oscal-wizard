9602e662aad3670ab80b25a031b63463881debc1
William Zujkowski <williamzujkowski@gmail.com>
2026-01-19 10:22:28 -0500

    feat: add validate and export pages (#47)

--- web/app.py
+++ web/app.py
@@ -10,10 +10,12 @@ from markdown_it import MarkdownIt
 from web.routes.control_interview import router as control_interview_router
 from web.routes.controls import router as controls_router
 from web.routes.diff_view import router as diff_router
+from web.routes.export_view import router as export_router
 from web.routes.home import router as home_router
 from web.routes.inventory import router as inventory_router
 from web.routes.parties import router as parties_router
 from web.routes.system_foundation import router as system_foundation_router
+from web.routes.validate_view import router as validate_router
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -32,3 +34,5 @@ app.include_router(diff_router)
 app.include_router(home_router)
 app.include_router(inventory_router)
 app.include_router(parties_router)
+app.include_router(validate_router)
+app.include_router(export_router)
